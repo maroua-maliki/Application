2a298d3c1ebba0d66dba184394e8d53edace3287
maliki <maroua.maliki@capgemini.com>
2025-07-18 10:12:32 +0100

    no

--- src/main/java/org/demo/demo/HelloApplication.java
+++ src/main/java/org/demo/demo/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
         stage.setTitle("KitChiffre");
         stage.setScene(scene);
         stage.show();
+        fxmlLoaderx
     }
 
     public static void main(String[] args) {
